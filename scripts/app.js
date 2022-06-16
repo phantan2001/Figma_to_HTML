8ca53f19d4f749632b5d3545e710b4f4b92d1f54
Phan Tan <pttan1304@gmail.com>
2022-06-16 15:19:15 +0700

    page1

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+$('.customer-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>"
+});
